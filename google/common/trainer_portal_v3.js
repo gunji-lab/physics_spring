b160712df0fa2846f672432b0678cbee3c9d5aa7
gunji-lab <gunji@toyo.jp>
2026-07-10 00:31:00 +0900

    Add heat gas written stage and progress

--- google/common/trainer_portal_v3.js
+++ google/common/trainer_portal_v3.js
@@ -35,7 +35,8 @@
     "heat_gas_stage2_framework_v1.html":"熱と気体/Stage2/シャルルの法則",
     "heat_gas_stage3_framework_v1.html":"熱と気体/Stage3/ボイル・シャルルの法則",
     "heat_gas_stage4_framework_v1.html":"熱と気体/Stage4/状態方程式",
-    "heat_gas_stage5_framework_v1.html":"熱と気体/Stage5/熱エネルギーと仕事"
+    "heat_gas_stage5_framework_v1.html":"熱と気体/Stage5/熱エネルギーと仕事",
+    "heat_gas_stage6_written_framework_v1.html":"熱と気体/Stage6/総合記述式"
   };
 
   function renderProgress(response) {
